43361ada8419cb901dc7f064c6f075daf29fd403
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-11-27 17:02:41 +0100

    Add logging

--- src/main/java/org/wicketstuff/datastore/memcached/MemcachedDataStore.java
+++ src/main/java/org/wicketstuff/datastore/memcached/MemcachedDataStore.java
@@ -2,6 +2,7 @@ package org.wicketstuff.datastore.memcached;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.net.SocketAddress;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
@@ -9,18 +10,25 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 
+import net.spy.memcached.ConnectionObserver;
 import net.spy.memcached.MemcachedClient;
 
+import net.spy.memcached.internal.OperationCompletionListener;
+import net.spy.memcached.internal.OperationFuture;
 import org.apache.wicket.pageStore.IDataStore;
 import org.apache.wicket.util.lang.Args;
 import org.apache.wicket.util.lang.Checks;
 import org.apache.wicket.util.time.Duration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * IDataStore that stores the data in Memcached
  */
 public class MemcachedDataStore implements IDataStore
 {
+	private static final Logger LOG = LoggerFactory.getLogger(MemcachedDataStore.class);
+
 	/**
 	 * A prefix for the keysPerSession to avoid duplication of keysPerSession
 	 * and to make it easier to find out who put the data
@@ -72,6 +80,22 @@ public class MemcachedDataStore implements IDataStore
 	{
 		this.client = Args.notNull(client, "client");
 		this.settings = Args.notNull(settings, "settings");
+
+		client.addObserver(new ConnectionObserver()
+		{
+			@Override
+			public void connectionEstablished(SocketAddress sa, int reconnectCount)
+			{
+				LOG.info("Established connection to: {}, reconnect count: {}", sa, reconnectCount);
+			}
+
+			@Override
+			public void connectionLost(SocketAddress sa)
+			{
+				LOG.warn("Lost connection to: {}", sa);
+			}
+		});
+
 	}
 
 	/**
@@ -93,7 +117,8 @@ public class MemcachedDataStore implements IDataStore
 
 		try
 		{
-			return new MemcachedClient(new InetSocketAddress(host, port));
+			MemcachedClient memcachedClient = new MemcachedClient(new InetSocketAddress(host, port));
+			return memcachedClient;
 		}
 		catch (IOException iox)
 		{
@@ -110,19 +135,36 @@ public class MemcachedDataStore implements IDataStore
 		if (keys != null && keys.contains(key))
 		{
 			bytes = (byte[]) client.get(key);
+
+			if (bytes == null)
+			{
+				// maybe the entry has expired
+				keys.remove(key);
+			}
 		}
+		LOG.debug("Got {} for session '{}' and page id '{}'",
+				new Object[] {bytes != null ? "data" : "'null'", sessionId, pageId});
 		return bytes;
 	}
 
 	@Override
-	public void removeData(String sessionId, int pageId)
+	public void removeData(final String sessionId, final int pageId)
 	{
-		Set<String> keys = keysPerSession.get(sessionId);
-		String key = getKey(sessionId, pageId);
+		final Set<String> keys = keysPerSession.get(sessionId);
+		final String key = getKey(sessionId, pageId);
+
 		if (keys != null && keys.contains(key))
 		{
-			client.delete(key);
-			keys.remove(key);
+			OperationFuture<Boolean> delete = client.delete(key);
+			delete.addListener(new OperationCompletionListener()
+			{
+				@Override
+				public void onComplete(OperationFuture<?> future) throws Exception
+				{
+					keys.remove(key);
+					LOG.debug("Removed the data for session '{}' and page id '{}'", sessionId, pageId);
+				}
+			});
 		}
 	}
 
@@ -137,13 +179,14 @@ public class MemcachedDataStore implements IDataStore
 				client.delete(key);
 			}
 			keysPerSession.remove(sessionId);
+			LOG.debug("Removed the data for session '{}'", sessionId);
 		}
 	}
 
 	@Override
-	public void storeData(String sessionId, int pageId, byte[] data)
+	public void storeData(final String sessionId, final int pageId, byte[] data)
 	{
-		String key = getKey(sessionId, pageId);
+		final String key = getKey(sessionId, pageId);
 		SortedSet<String> keys = keysPerSession.get(sessionId);
 		if (keys == null)
 		{
@@ -154,7 +197,6 @@ public class MemcachedDataStore implements IDataStore
 				keys = old;
 			}
 		}
-		keys.add(key);
 
 		Duration expirationTime = settings.getExpirationTime();
 
@@ -162,13 +204,25 @@ public class MemcachedDataStore implements IDataStore
 		// See net.spy.memcached.MemcachedClient.set(java.lang.String, int, java.lang.Object)()
 //		Time timeToExpire = Time.now().add(expirationTime);
 
-		client.set(key, (int) expirationTime.getMilliseconds(), data);
+		final SortedSet<String> _keys = keys;
+
+		OperationFuture<Boolean> set = client.set(key, (int) expirationTime.getMilliseconds(), data);
+		set.addListener(new OperationCompletionListener()
+		{
+			@Override
+			public void onComplete(OperationFuture<?> future) throws Exception
+			{
+				_keys.add(key);
+				LOG.debug("Stored data for session '{}' and page id '{}'", sessionId, pageId);
+			}
+		});
 	}
 
 	@Override
 	public void destroy()
 	{
 		Duration timeout = settings.getShutdownTimeout();
+		LOG.info("Shutting down gracefully for {}", timeout);
 		client.shutdown(timeout.getMilliseconds(), TimeUnit.MILLISECONDS);
 	}
 
